fcf6985d5e143f4de4f4c206ce386ca47f385aa0
Aero <you@example.com>
2024-12-23 13:16:29 +0700

    0.0.0

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
-import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,11 @@
 import MobileNavBar from "./MobileNavBar";
 import DesktopNavBar from "./DesktopNavBar";
+import Image from "next/image";
 
 function Header() {
   return (
     <header className="mb-8 flex items-center justify-between">
-      <img src="/assets/images/logo.svg" alt="logo" />
+      <Image src="/assets/images/logo.svg" alt="logo" />
       <MobileNavBar />
       <DesktopNavBar />
     </header>
